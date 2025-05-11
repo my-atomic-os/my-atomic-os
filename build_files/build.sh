79de8f33c767481ea7bde4de895d424d1c752646
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-11 18:42:25 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,37 +16,11 @@ log "setting up repos"
 log "installig packages"
 /ctx/packages.sh
 
-### fix signing
-restorecon -RFv /etc/pki/containers
-bash -c 'cat <<EOF > /etc/containers/registries.d/ghcr.tammam20.my-atomic-os.yaml
-docker:
-  ghcr.io/tammam20/my-atomic-os:
-    use-sigstore-attachments: true
-EOF'
+log "setting up services"
+/ctx/services.sh
 
-cat <<EOF > /etc/containers/policy.json
-{
-    "default": [
-        {
-            "type": "reject"
-        }
-    ],
-    "transports": {
-        "docker": {
-            "ghcr.io/tammam20/my-atomic-os": [
-                {
-                    "type": "sigstoreSigned",
-                    "keyPath": "/etc/pki/containers/cosign.pub",
-                    "signedIdentity": {
-                        "type": "matchRepository"
-                    }
-                }
-            ],
-            "": [{"type": "insecureAcceptAnything"}]
-	},
-        "docker-daemon": {
-	        "": [{"type": "insecureAcceptAnything"}]
-	    }
-    }
-}
-EOF
+log "fixing signing"
+/ctx/signing.sh
+
+log "clean build env"
+/ctx/cleanup.sh
