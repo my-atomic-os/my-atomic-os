03404434e3306e80c5a9b4ac4cba63a014e263f4
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-11 18:39:23 +0300

    Create packages.sh

---
+++ build_files/packages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+set ${SET_X:+-x} -eou pipefail
+
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
+
+log() {
+  echo "=== $* ==="
+}
+
+log "installing packages"
+
+dnf5 -y group install --with-optional virtualization
+dnf5 install -y mozilla-openh264 \
+	open-fprintd \
+	fprintd-clients \
+	fprintd-clients-pam \
+	python3-validity \
+ 	code \
+   	rust cargo clippy rustfmt \
+    	rust-src \
+    	lm_sensors stress-ng \
+    	rpmfusion-\*-appstream-data \
+     	intel-media-driver \
+      	python3-pip rclone \
+       	chromium \
+	distrobox gh \
+ 	"https://github.com/twpayne/chezmoi/releases/download/v2.62.2/chezmoi-2.62.2-x86_64.rpm" \
+  	cmake glibc-devel \
+   	fastfetch
+       
+     
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
