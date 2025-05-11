b40694205378b766c8ac1c5de201dc09368183db
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-11 19:09:53 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,23 +11,18 @@ log() {
 log "setting up my-atomic-os build proccess"
 
 log "setting up repos"
-chmod +x /ctx/repo.sh
 /ctx/repo.sh
 
 log "installig packages"
-chmod +x /ctx/packages.sh
 /ctx/packages.sh
 
 log "setting up services"
-chmod +x /ctx/services.sh
 /ctx/services.sh
 
 log "fixing signing"
-chmod +x /ctx/signing.sh
 /ctx/signing.sh
 
 log "clean build env"
-chmod +x /ctx/cleanup.sh
 /ctx/cleanup.sh
 
 log "build is successful"
