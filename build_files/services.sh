e54918bd8bc2eb61bd3b9cae3e0e4852bbf24492
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-11 18:48:36 +0300

    Update services.sh

--- build_files/services.sh
+++ build_files/services.sh
@@ -14,8 +14,7 @@ systemctl enable podman.socket \
 	python3-validity.service \
 	open-fprintd-restart-after-resume.service \
 	python3-validity-restart-after-resume.service \
-	libvirtd.service \
-  	chronyd.service
+	libvirtd.service
 
 log "disable services"
 systemctl disable open-fprintd-resume.service \
