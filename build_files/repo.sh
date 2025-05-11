5664ac01df4674ec76e326a897893c9e798e6957
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-11 18:29:08 +0300

    Update repo.sh

--- build_files/repo.sh
+++ build_files/repo.sh
@@ -2,3 +2,18 @@
 
 set -ouex pipefail
 
+### setup repos
+rpm  --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+autorefresh=1
+type=rpm-md
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | \
+sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf5 -y copr enable sneexy/python-validity
+dnf5 -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
