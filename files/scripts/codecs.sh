53f667394112c44abfc5b08934d5df8ddfdb06b7
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-05-13 19:39:27 +0300

    hello

---
+++ files/scripts/codecs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set ${SET_X:+-x} -eou pipefail
+
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
+
+log() {
+  echo "=== $* ==="
+}
+
+log "installing packages"
+     
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
