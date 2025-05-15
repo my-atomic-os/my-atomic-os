33091f65a51f61b1128a6c0ac760ba15fb5d42fa
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-05-15 18:19:24 +0300

    update

--- files/scripts/codecs.sh
+++ files/scripts/codecs.sh
@@ -12,6 +12,3 @@ log "installing packages"
      
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-lpf approve xone-firmware
-lpf build xone-firmware
-lpf install xone-firmware
