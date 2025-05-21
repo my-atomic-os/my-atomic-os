5d7a2eb85917940ef225d26b121dd7f13560d36b
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-05-21 18:12:11 +0300

    update

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -12,36 +12,40 @@ log "installing packages"
 
 dnf5 -y group install --with-optional virtualization 
 dnf5 install -y mozilla-openh264 \
-		open-fprintd \
-		fprintd-clients \
-		fprintd-clients-pam \
-		python3-validity \
- 		code \
-   		rust cargo clippy rustfmt \
-    		rust-src \
-    		lm_sensors stress-ng \
-    		rpmfusion-\*-appstream-data \
-    		intel-media-driver \
-    		python3-pip rclone \
-    		chromium \
-		distrobox gh \
- 		chezmoi \
-  		cmake glibc-devel \
-   		fastfetch \
-		gnome-backgrounds-extras \
- 		gnome-themes-extra \
-  		gnome-tweaks
+				open-fprintd \
+				fprintd-clients \
+				fprintd-clients-pam \
+				python3-validity \
+ 				code \
+   				rust cargo clippy rustfmt \
+    			rust-src \
+    			lm_sensors stress-ng \
+    			rpmfusion-\*-appstream-data \
+    			intel-media-driver \
+    			python3-pip rclone \
+    			chromium \
+				distrobox gh \
+ 				chezmoi \
+  				cmake glibc-devel \
+   				fastfetch \
+				gnome-backgrounds-extras \
+ 				gnome-themes-extra \
+  				gnome-tweaks
 
-dnf5 remove -y fedora-workstation-backgrounds \
-	       desktop-backgrounds-gnome \
-               f42-backgrounds-gnome \
-               gnome-classic-session \
-               gnome-shell-extension-apps-menu \
-               gnome-shell-extension-launch-new-instance \
-               gnome-shell-extension-places-menu \
-               gnome-shell-extension-window-list \
-	       gnome-shell-extension-background-logo \
-	       gnome-extensions-app
+dnf5 remove -y  fedora-workstation-backgrounds \
+	       	    desktop-backgrounds-gnome \
+                f42-backgrounds-gnome \
+                gnome-classic-session \
+                gnome-shell-extension-apps-menu \
+                gnome-shell-extension-launch-new-instance \
+                gnome-shell-extension-places-menu \
+                gnome-shell-extension-window-list \
+	       	    gnome-shell-extension-background-logo \
+	            gnome-extensions-app
        
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+adduser --system dhcpcd --user-group
+adduser --system gluster --user-group
+groupadd --system libvirt
+groupadd --system qat
