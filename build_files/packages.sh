96aadd4641f199cca58f5cc0bbb968c16c14683e
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-21 17:21:42 +0300

    Update packages.sh

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -12,35 +12,36 @@ log "installing packages"
 
 dnf5 -y group install --with-optional virtualization 
 dnf5 install -y mozilla-openh264 \
-	open-fprintd \
-	fprintd-clients \
-	fprintd-clients-pam \
-	python3-validity \
- 	code \
-   	rust cargo clippy rustfmt \
-    rust-src \
-    lm_sensors stress-ng \
-    rpmfusion-\*-appstream-data \
-    intel-media-driver \
-    python3-pip rclone \
-    chromium \
-	distrobox gh \
- 	chezmoi \
-  	cmake glibc-devel \
-   	fastfetch \
-	gnome-backgrounds-extras \
- 	gnome-themes-extra \
-  	gnome-tweaks
+		open-fprintd \
+		fprintd-clients \
+		fprintd-clients-pam \
+		python3-validity \
+ 		code \
+   		rust cargo clippy rustfmt \
+    		rust-src \
+    		lm_sensors stress-ng \
+    		rpmfusion-\*-appstream-data \
+    		intel-media-driver \
+    		python3-pip rclone \
+    		chromium \
+		distrobox gh \
+ 		chezmoi \
+  		cmake glibc-devel \
+   		fastfetch \
+		gnome-backgrounds-extras \
+ 		gnome-themes-extra \
+  		gnome-tweaks
 
 dnf5 remove -y fedora-workstation-backgrounds \
-		desktop-backgrounds-gnome \
-        f42-backgrounds-gnome \
-        gnome-classic-session \
-        gnome-shell-extension-apps-menu \
-        gnome-shell-extension-launch-new-instance \
-        gnome-shell-extension-places-menu \
-        gnome-shell-extension-window-list
+	       desktop-backgrounds-gnome \
+               f42-backgrounds-gnome \
+               gnome-classic-session \
+               gnome-shell-extension-apps-menu \
+               gnome-shell-extension-launch-new-instance \
+               gnome-shell-extension-places-menu \
+               gnome-shell-extension-window-list \
+	       gnome-shell-extension-background-logo \
+	       gnome-extensions-app
        
-     
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
