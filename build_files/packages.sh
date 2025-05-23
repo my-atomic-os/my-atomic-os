b701bd297640be05cde7da06322a135d60931260
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-05-23 12:42:47 +0300

    Update packages.sh

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -10,45 +10,24 @@ log() {
 
 log "installing packages"
 
-
-dnf5 -y group install development-tools
-dnf5 -y group install --with-optional virtualization 
+dnf5 -y group install virtualization 
 dnf5 install -y mozilla-openh264 \
-			open-fprintd \
-			fprintd-clients \
-			fprintd-clients-pam \
-			python3-validity \
- 			code \
-   			rust cargo clippy rustfmt \
-    			rust-src \
-    			lm_sensors stress-ng \
-    			rpmfusion-\*-appstream-data \
-    			intel-media-driver \
-    			python3-pip rclone \
-    			chromium \
-			distrobox gh \
- 			chezmoi \
-  			cmake glibc-devel \
-   			fastfetch \
-			gnome-backgrounds-extras \
- 			gnome-themes-extra \
-  			gnome-tweaks \
-      			automake autoconf \
-	 		cups-devel
-
-dnf5 remove -y  fedora-workstation-backgrounds \
-	       	desktop-backgrounds-gnome \
-                f42-backgrounds-gnome \
-                gnome-classic-session \
-                gnome-shell-extension-apps-menu \
-                gnome-shell-extension-launch-new-instance \
-                gnome-shell-extension-places-menu \
-                gnome-shell-extension-window-list \
-	       	gnome-shell-extension-background-logo \
-	     	fedora-bookmarks \
-       		fedora-chromium-config \
-	 	fedora-chromium-config-gnome \
-   		gnome-software-rpm-ostree
+		open-fprintd \
+		fprintd-clients \
+		fprintd-clients-pam \
+		python3-validity \
+ 		code \
+    		lm_sensors \
+      		stress-ng \
+    		intel-media-driver \
+    		python3-pip \
+    		chromium \
+		distrobox \
+  		gh \
+		chezmoi \
+   		fastfetch \
+ 		gnome-themes-extra \
+  		gnome-tweaks
        
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
